e26642a85a2079cbf671f14fd2e3356a6457f3d5
Aditya-droid-byte <srivastavaadi246@gmail.com>
2025-11-29 00:55:17 +0530

    Removed sensitive data

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,15 @@
 const User = require("../models/user");
 const encrypt = require("bcryptjs");
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "srivastavaadi247@gmail.com",
+    pass: "***REMOVED***"
+  }
+});
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get('Cookie').split('; ')[0].trim().split('=')[1];
   // console.log(isLoggedIn);
@@ -34,11 +44,15 @@ exports.postLogin = (req, res, next) => {
             req.session.user = user;
             return req.session.save((err) => {
               console.log(err);
-              return res.redirect("/");
+              transporter.sendMail({
+                to: email,
+                subject: 'login success at MangaStore',
+                html: '<h1> You have logged in successfully <h1>'
+              })
+              res.redirect("/");
             });
           }
           req.flash("error", "Invalid email or password");
-
           res.redirect("/login");
         })
         .catch((err) => {
@@ -60,9 +74,9 @@ exports.postLogout = (req, res, next) => {
 
 exports.getSignup = (req, res, next) => {
   let message = req.flash('error');
-  if(message.length > 0){
+  if (message.length > 0) {
     message = message[0];
-  }else{
+  } else {
     message = null;
   }
   res.render("auth/signup", {
@@ -97,10 +111,121 @@ exports.postSignup = (req, res, next) => {
           return user.save();
         })
         .then((result) => {
-          res.redirect("/login");
-        });
+          transporter.sendMail({
+            to: email,
+            subject: 'SignUp succeeded at mangaStore',
+            html: '<h1> You have signed up successfully <h1>'
+          })
+          return res.redirect("/login");
+        }).catch(err => console.log(err));
     })
     .catch((err) => {
       console.log(err);
     });
 };
+
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: 'reset',
+    pageTitle: 'Reset Password',
+    isAuthenticated: req.session.isUserLoggedIn,
+    errorMessage: message
+  })
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const email = req.body.email;
+    const token = buffer.toString('hex');
+    User.findOne({ email: email }).
+      then(user => {
+        if (!user) {
+          req.flash('error', "No account with this user is found");
+          res.redirect('/reset');
+          return Promise.reject("USER_NOT_FOUND: " + email);
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      }).then(result => {
+        res.redirect('/');
+        return transporter.sendMail({
+          to: email,
+          subject: 'Reset password',
+          html: `
+            <p> You requested for password reset of your MangaStore account. If not by you please ignore this email</p>
+            <p> Click the <a href="http://localhost:4200/reset/${email}/${token}">link</a> to reset your password </p>
+            `
+        })
+      }).
+      catch((err) => {
+        console.log(err);
+      });
+  });
+}
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  const email = req.params.email;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } }).
+    then(user => {
+      let message = req.flash('error');
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'Update password',
+        errorMessage: message,
+        userId: user._id.toString(),
+        isAuthenticated: req.session.isUserLoggedIn,
+        passwordToken: token,
+        email: email
+      })
+    }).
+    catch(err => console.log(err));
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  const email = req.body.email;
+  console.log("email: "+ email);
+  let resetUser;
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId
+  }).then(user => {
+    resetUser = user;
+    return encrypt.hash(newPassword, 12);
+  }).then(hashedPassword => {
+    resetUser.password = hashedPassword;
+    resetUser.resetToken = null;
+    resetUser.resetTokenExpiration = undefined;
+    return resetUser.save();
+  }).then(result => {
+    console.log('password updated successfullu');
+    res.redirect('/login');
+    return transporter.sendMail({
+      to: email,
+      subject: 'Reset password',
+      html: 'Your password has been updated successfully'
+    })
+  })
+    .catch(err => console.log(err));
+}
